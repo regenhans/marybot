888607fb10443ab782955fbaa25153c5703c1826
regenhans <hnsdream@gmail.com>
2016-08-18 11:17:13 -0500

    env.vars

--- marybot.js
+++ marybot.js
@@ -2,11 +2,13 @@
 var Twit = require('twit'),
 	config = require('./config'),
 	T = new Twit(config);
-// search for the following
 
+//call function once
 trollAle();
+//then call it every minute
 setInterval(trollAle, 1000*60);
 
+//twit function
 function trollAle (){
    var randomNumber = Math.floor(Math.random()*(1000-1));
    T.post('statuses/update', { status: ' Sabían que @basicavisual se llama https://goo.gl/ThosxD ' + randomNumber }, function(err, data, response) {
