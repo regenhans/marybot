17f95aaef9237cf444293be9a74631e9139834a5
regenhans <hnsdream@gmail.com>
2016-08-23 16:27:43 -0500

    mary 1.0

--- marybot.js
+++ marybot.js
@@ -5,14 +5,15 @@ var Twit = require('twit'),
 
 //call function once
 trollAle();
-//then call it every minute
-setInterval(trollAle, 1000*60);
+//then call it every minute (1000ms*60 = 60 seconds)
+setInterval(troll, 1000*60);
 
 //twit function
-function trollAle (){
+function troll (){
+	//we need a random number because we can't have the exact same tweet twice
    var randomNumber = Math.floor(Math.random()*(1000-1));
+	//here we compose the tweet (in this case I'm tweeten my friend's 'secret' name)
    T.post('statuses/update', { status: ' Sabían que @basicavisual se llama https://goo.gl/ThosxD ' + randomNumber }, function(err, data, response) {
       console.log(data)
    })
-
 }
